e18763d69873b4a5fffd562223fc35e4175b29e8
kamil <karolakkamil321@gmail.com>
2019-07-10 15:12:35 +0200

    user index page is set up

--- spec/features/user_index_spec.rb
+++ spec/features/user_index_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-feature "User index page", :devise do
+feature 'User index page', :devise do
   scenario 'user sees own email address' do
     user = FactoryGirl.create(:user, :admin)
-    login_as(user,scope: :user)
+    login_as(user, scope: :user)
     visit users_path
     expect(page).to have_content user.email
   end
